c94820012ebc7dbf14ac85fcb13f11c0968a0bf5
arol1234 <97754141+arol1234@users.noreply.github.com>
2022-05-08 22:53:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+searchIcon.addEventListener("click", () => {
+  searchIcon.parentElement.classList.add("change");
+  searchInputWrapper.classList.add("change");
+
+  setTimeout(() => {
+    searchInput.focus();
+  }, 1000);
+});
+
+closeIcon.addEventListener("click", () => {
+  searchIcon.parentElement.classList.remove("change");
+  searchInputWrapper.classList.remove("change");
+});
